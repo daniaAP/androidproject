4fda3d2504ebc9177cefc615c8f94b5e308028e6
daniaAP <daniaaasi1994@gmail.com>
2023-09-18 09:58:31 +0300

    first page

--- app/src/main/java/com/example/projdania/MainActivity.java
+++ app/src/main/java/com/example/projdania/MainActivity.java
@@ -1,28 +1,78 @@
 package com.example.projdania;
 
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    Button loginButton;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loginButton=findViewById(R.id.loginBT);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+
+        Toolbar toolbar = findViewById(R.id.toolbar); //Ignore red line errors
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,
+                R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new home()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            if(R.id.nav_home==item.getItemId()){
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new home()).commit();
+
             }
-        });
+            else if(R.id.nav_messages==item.getItemId()){
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new messages()).commit();
+            }
+            else if(R.id.nav_contact==item.getItemId()){
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new fyp()).commit();
+            }
+            else if(R.id.nav_info==item.getItemId()){
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new profile()).commit();
+            }
+            else if(R.id.nav_logout==item.getItemId()){
+                startActivity(new Intent(this,LoginActivity.class));
+                Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show();
+            }
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        }
 
 
-
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
